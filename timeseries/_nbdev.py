c3daec149052b8e4e6064c27aba29922ef7ec687
Farid Hassainia <fh@itecpedia.com>
2020-02-10 11:46:10 -0500

    Moved get_ts_items to data module

--- timeseries/_nbdev.py
+++ timeseries/_nbdev.py
@@ -3,13 +3,13 @@
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"TSData": "80_timeseries_data.ipynb",
+         "get_ts_items": "80_timeseries_data.ipynb",
          "show_timeseries": "80_timeseries_data.ipynb",
          "download_unzip_data": "80_timeseries_data.ipynb",
          "unzip": "80_timeseries_data.ipynb",
          "download_unzip_data_UCR": "80_timeseries_data.ipynb",
          "get_UCR_univariate_list": "80_timeseries_data.ipynb",
          "get_UCR_multivariate_list": "80_timeseries_data.ipynb",
-         "get_ts_items": "81_timeseries_core.ipynb",
          "TensorTS": "81_timeseries_core.ipynb",
          "ToTensorTSBlock": "81_timeseries_core.ipynb",
          "ToTensorTS": "81_timeseries_core.ipynb",
@@ -22,7 +22,7 @@ index = {"TSData": "80_timeseries_data.ipynb",
          "get_grid": "81_timeseries_core.ipynb",
          "show_results": "81_timeseries_core.ipynb"}
 
-modules = ["tsdata.py",
+modules = ["data.py",
            "core.py"]
 
 doc_url = "https://ai-fast-track.github.io/timeseries/"
